96c44c55fdbbc9670d45aeafd203ea48ab92fc87
Haleem Bello <hbo@Haleems-MacBook-Air.local>
2023-10-03 10:49:34 -0400

    chat ongoing

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useAtlasUser } from '@/context/UserContext';
-import { useEffect, useState } from 'react';
+import { useError } from '@/context/ErrorContext';
+import { useEffect, useState, useRef } from 'react';
 import { redirect } from 'next/navigation';
 import { Menu, Settings, Logout, Send } from '@mui/icons-material';
 import Sidebar from '../components/dashboard/Sidebar';
@@ -10,13 +11,50 @@ import { InputBase, IconButton, Paper } from '@mui/material';
 
 export default function Chat(): JSX.Element {
   const { user } = useAtlasUser();
-  const [open, setOpen] = useState(false);
+  const { setError } = useError();
+  const [open, setOpen] = useState(false); // sidebar
+  // chat state
+  const [messages, setMessages] = useState<any[]>([]);
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // send message
+  const sendMessage = async (e: any) => {
+    e.preventDefault();
+    if (loading) return; // prevent spamming
+    if (message === '') return; // prevent empty messages
+    setLoading(true); // set loading
+    setMessages([...messages, { message, isUser: true }]); // add message to chat
+    setMessage(''); // reset message
+    try {
+      const res = await fetch('/api/chat', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.message);
+      }
+      setMessages([...messages, { message: data.message, isUser: false }]);
+    } catch (err) {
+      console.error(err);
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // wait for user to load
   useEffect(() => {
-    if (!user?.isSuscribed) {
+    // wait for user to load
+    if (!user) return;
+    if (user.isSuscribed === false) {
       redirect('/dashboard');
     }
   }, [user]);
+
   return (
     <>
       <nav className="p-6 w-full h-fit flex flex-row justify-between">
@@ -44,11 +82,42 @@ export default function Chat(): JSX.Element {
       </nav>
       <div className="flex flex-col h-full w-full">
         <div className="flex flex-col h-full w-full bg-off-white p-10">
-          <div className="h-full w-full flex items-center justify-center">
-            <h3 className="rounded-md ring-2 ring-offset-2 ring-gray-400 ring-opacity-50 p-4">
-              My car wont start, what could be the problem?
-            </h3>
-          </div>
+          {messages.length > 0 ? (
+            <div className="h-full w-full">
+              {messages.map((msg, i) => (
+                <div
+                  key={i}
+                  className={`flex flex-row p-2 m-3 mb-5 ${
+                    msg.isUser ? 'justify-end' : ''
+                  }`}
+                >
+                  <div
+                    className={`rounded-lg rounded-br-none ring-1 ring-offset-2 ring-gray-400 ring-opacity-50 p-4 bg-primary text-white`}
+                  >
+                    {msg.message}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="h-full w-full flex items-center justify-center">
+              <h3 className="rounded-md ring-2 ring-offset-2 ring-gray-400 ring-opacity-50 p-4">
+                My car won't start, what could be the problem?
+                <IconButton
+                  className="ml-4"
+                  aria-label="send"
+                  onClick={() => {
+                    inputRef.current?.focus();
+                    setMessage(
+                      "My car won't start, what could be the problem?"
+                    );
+                  }}
+                >
+                  <Send />
+                </IconButton>
+              </h3>
+            </div>
+          )}
         </div>
         <div className="absolute bottom-0 h-fit w-full bg-white">
           <Paper
@@ -58,11 +127,18 @@ export default function Chat(): JSX.Element {
             <InputBase
               className="flex-grow px-4"
               placeholder="Type here..."
-              inputProps={{ 'aria-label': 'type here' }}
+              inputProps={{ 'aria-label': 'type here', ref: inputRef }}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') sendMessage(e);
+              }}
             />
             <IconButton
               type="submit"
               className="mr-4"
+              aria-label="send"
+              onClick={sendMessage}
             >
               <Send />
             </IconButton>
